dddde405539cb42777d0c522a32b74e93c537c79
Daniel Werner <danielcswerner@gmail.com>
2017-04-22 16:30:40 -0400

    Extracted the parts appearing underneath the navigation section and made them depend upon the current rout; new module for the main page content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainContentComponent } from './main-content.component';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/main',
+        pathMatch: 'full'
+      },
+      {
+        path: 'main',
+        component: MainContentComponent
+      }
+    ]) 
+  ],
+  declarations: [ 
+    AppComponent,
+    MainContentComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
