f41e59bb65de14e85eec557b3a7f1103a6638ffb
Daniel Werner <danielcswerner@gmail.com>
2017-04-29 16:46:31 -0400

    Tabs component now subscribes to router events and makes the right tab active.  This will allow the application to change routes from anywhere (like in a link outside of the nav bar) and still have the nav bar react correctly

--- src/app/navbar/tabs.component.ts
+++ src/app/navbar/tabs.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: 'wernerware-tabs',
   templateUrl: './tabs.component.html'
 })
-export class WernerwareTabs  {  }
+export class WernerwareTabs implements OnInit {
+    tabs = TABS;
+    constructor(private router: Router){}
+    ngOnInit(): void {
+        this.router.events.subscribe((event) =>{
+            if( event instanceof NavigationEnd ){
+                let url = event.urlAfterRedirects;
+                console.log(event);
+                let selected = TABS.filter((tab) => {return tab.link === url});
+                if( selected.length > 0 ){
+                    TABS[active].css = '';
+                    active = selected[0].index;
+                    selected[0].css = 'active';
+                }
+            }
+        });
+    }
+}
+
+class WernerwareTab {
+    index: number;
+    label: string;
+    link: string;
+    css: string;
+}
+
+const TABS: WernerwareTab[] = [
+    {index: 0, label: 'Home', link: '/main', css: 'active'},
+    {index: 1, label: 'About', link: '/about', css: ''},
+    {index: 1, label: 'Projects', link: '/projects', css: ''},
+    {index: 1, label: 'Contact', link: '/contact', css: ''}
+];
+
+let active: number = 0;
